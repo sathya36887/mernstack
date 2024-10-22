170f189204a2cd5438e19db81c9eccef69bad4bb
I SIVASATHYA <146161270+sathya36887@users.noreply.github.com>
2024-10-22 11:26:05 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,19 @@
+//1.print hello world using js
+console.log("hello world");
+// 2.declaring variables
+
+var a= 3;
+function declaration(){
+    var b=6;
+    let a = 7;
+    console.log(a,b);
+}
+declaration();
+console.log(a);
+//3.eligible for vote or not
+let age =
+if (age >= 18) {
+  console.log("You are eligible to vote!");
+} else {
+  console.log("You are not eligible to vote yet!");
+}
